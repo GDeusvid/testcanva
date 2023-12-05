cb178c873164bc6542808a077b9e3b7478cc6b9c
GDeusvid <gdavidgsnow@gmail.com>
2023-12-05 08:36:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+
+// Exemplo: Desenhar um retângulo preenchido
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 100, 75);
